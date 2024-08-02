a35a782037947f22c0ae0f9ab843888ede86ec32
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-02 17:37:06 +0530

    Add files via upload

---
+++ TakingInput.py
@@ -0,0 +1,13 @@
+#Day 2 of python coding
+
+
+#taking input
+
+print("Hello candidate!")
+print('Type your name, please')
+_name=input()
+print('Your name is',_name)
+candidate_Age=input('Type your age: ')
+print(_name,'\'s age is',candidate_Age)
+print('There is a gap between name and \'s; to correct it')
+print(_name +'\'s age is',candidate_Age)
