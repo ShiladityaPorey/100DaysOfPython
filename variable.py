5d60b3335df9ee37a7a7d8db6f5fafa06c3c1d2f
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-03 09:23:53 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,26 @@
+#Day3 of python coding
+
+print("===================")
+print("Rules for variable names :\n")
+print("1. One word.\n")
+print("2. We can use only letters, numbers, and underscrore (_).\n")
+print("3. We can not begin name with number.\n")
+print("4. Case sensitive: ‘sPam’ and ‘spam’ are different variable names.")
+print("===================")
+
+
+print("Whenever we store variable as input(), we store it as string.\n")
+print("Add your age")
+AgE=input()
+print("Your age:",AgE)
+
+print("Next year your age will be: Your age +1")
+print("Can we get output if we print int(AgE)+1 ?", int(AgE)+1)
+
+print("Note: Adding 1 to a string will cause an error, so following line will not work as intended.")
+
+print("Your age +1 (calculating using AgE+1)")
+
+print(AgE+1)
+
+
