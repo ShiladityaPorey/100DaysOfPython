fb7ebaa6dfc528612ae42a29689ee978063d6687
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-04 09:55:57 +0530

    Add files via upload

---
+++ integer_string_data_types.py
@@ -0,0 +1,34 @@
+#Day3 of python coding
+
+#August 4, 2024
+
+
+print("==== Additional Clarification on Day 2 Programming ====")
+print("When you define 'aGe = 32' or 'Age9_in = int(input())', the data type is integer in Python.")
+print("But when you define 'age2d = input()', the data type is string in Python.")
+
+
+print("For example, we have defined 'aGe = 32'.")
+
+# Defining variables
+aGe = 32
+
+
+print("Now, assign an integer value to the variable 'Age9_in':")
+
+# Assigning integer value to 'Age9_in' through user input
+Age9_in = int(input())
+
+
+print("Now, assign an integer value to the variable 'age2d':")
+
+# Assigning string value to 'age2d' through user input
+age2d = input()
+
+
+
+print("We will print output using this command:")
+#print("print(aGe,"\n",Age9_in,"\n",age2d)")
+print(aGe,"\n",Age9_in,"\n",age2d)
+
+
