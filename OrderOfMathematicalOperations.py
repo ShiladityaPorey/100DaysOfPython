4713ad572b8f764a84eb802367be5b96bff7d577
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-05 17:55:54 +0530

    Add files via upload

---
+++ OrderOfMathematicalOperations.py
@@ -0,0 +1,28 @@
+#Day5 of Python coding
+
+print("Enter three integer number")
+Number_1 = int(input("Enter first integer number: "))
+Number_2 = int(input("Enter second integer number: "))
+Number_3 = int(input("Enter third integer number: "))
+print("\n")
+
+print("(Number_1+1)**Number_2",(Number_1+1)**Number_2)
+print("Number_1**(Number_2+1)",Number_1**(Number_2+1),"\n")
+
+
+print("Number_1**Number_2*Number_3",Number_1**Number_2*Number_3)
+print("Number_1*Number_2**Number_3",Number_1*Number_2**Number_3,"\n")
+
+
+print("Number_1**Number_2/Number_3",Number_1**Number_2/Number_3)
+print("Number_1/Number_2**Number_3",Number_1/Number_2**Number_3,"\n")
+
+
+print("Number_1*Number_2/Number_3",Number_1*Number_2/Number_3)
+print("Number_1/Number_2*Number_3",Number_1/Number_2*Number_3,"\n")
+
+print("Number_1/Number_2+Number_3",Number_1/Number_2+Number_3)
+print("Number_1+Number_2/Number_3",Number_1+Number_2/Number_3,"\n")
+
+print("Number_1+Number_2-Number_3",Number_1+Number_2-Number_3)
+print("Number_1-Number_2+Number_3",Number_1-Number_2+Number_3,"\n")
