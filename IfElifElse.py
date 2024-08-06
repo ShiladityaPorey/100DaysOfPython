3e608492aba869136d2abe0dd1878029636ace8a
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-06 10:48:14 +0530

    Add files via upload

---
+++ IfElifElse.py
@@ -0,0 +1,14 @@
+#Day6 of python coding
+#August 6, 2024
+
+Name_1='A'
+Name_2='B'
+
+Name_=input("Enter your name ",)
+
+if Name_1 == Name_:
+	print('Hi',Name_)
+elif Name_2 == Name_:
+	print('Hi',Name_)
+else:
+	print('Your name in not in our list.')
