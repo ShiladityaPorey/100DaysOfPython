56de0715de78e18e8a39f5443fc238a301a9d287
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-06 10:49:12 +0530

    Add files via upload
    This is a code with an error.
    Indentation is crucial in Python. Incorrect indentation can cause errors.

---
+++ indentionIMPOSRTANT.py
@@ -0,0 +1,13 @@
+#august 6, 2024
+
+Name_1='A'
+Name_2='B'
+
+Name_=input("Enter your name ",)
+
+if Name_1 == Name_:
+        print('Hi',Name_)
+elif Name_2 == Name_:
+print('Hi',Name_)
+else:
+	print('Your name in not in our list.')
