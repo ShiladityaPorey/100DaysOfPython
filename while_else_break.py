fdcc27b137277f5292a09d6007efccb87746d0a0
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-07 10:25:50 +0530

    Add files via upload
    ▶ else is not mandatory after a while loop.
    ▶ The else block executes only if the loop completes without interruption. If a break statement is encountered, the else block is skipped.

---
+++ while_else_break.py
@@ -0,0 +1,11 @@
+#Day 7 of python coding
+#August 7, 2024
+
+i = 0
+while i < 5:
+    if i == 3:
+        break
+    print(i)
+    i += 1
+else:
+    print("Loop finished without interruption.")  # This line will not be executed.
