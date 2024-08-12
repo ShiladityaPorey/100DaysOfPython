5452b220ab9c5ca507256fb6877003d4dc262793
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-12 20:25:24 +0530

    Add example demonstrating f-strings in Python
    - Included a script showing basic usage of f-strings for string formatting
    - Added examples for output formatting, variable inclusion, and arithmetic operations

---
+++ day12_fstrings_demo.py
@@ -0,0 +1,31 @@
+#Day 12 of python coding
+#August 12, 2024
+
+print("Hello friends!")
+
+print(f"Hello friends!")
+
+
+INPUT = int(input("Enter an integer number: "))
+#f-strings are generally used for formatting output rather than input prompts.
+#For input prompts, you don't need to use f-strings.
+#If you still want to include a variable within an f-string
+#here's how you can do it:
+INPUT= int(input(f"Enter an integer number: "))
+
+
+Name='Vikram'
+age=35
+
+print(Name + "\'s age is ",age)
+print(f"{Name}'s age is {age}.")
+
+
+
+print("sum of 50+60+70 is", 50+60+70)
+print(f"sum of 50+60+70 is {50+60+70}")
+
+
+
+print("Let me tell you a story.\n")
+print(f"Let me tell you a story.\n")
