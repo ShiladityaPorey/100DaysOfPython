6352226845b971402cf127cd043d5aa4cddddd66
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-30 04:26:38 +0530

    Added Day 23 Python code to demonstrate accessing elements from a nested list.

---
+++ day23_nested_list_access.py
@@ -0,0 +1,19 @@
+# Day 23 Python Programming
+# August 23, 2024
+
+dl=[[0,1,2],['PY1','PY2','PY3'],['A','B','C']]
+
+print(dl)
+
+
+# Accessing elements in nested lists
+print(dl[0][1]) # Output: 1
+
+print(dl[1][2]) # Output: 'PY3'
+
+
+print(dl[2][2]) # Output: 'C'
+
+
+
+
