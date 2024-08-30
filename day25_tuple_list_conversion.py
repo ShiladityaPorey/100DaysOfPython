a8f741e85376397472bb56a3af74e1a4e3937e78
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-08-30 12:43:43 +0530

    Day 25: Convert between tuples and lists in Python
    - Added code to demonstrate conversion from tuple to list and list to tuple.
    - Included type checks to confirm data type changes.

---
+++ day25_tuple_list_conversion.py
@@ -0,0 +1,22 @@
+#Day25 of Python programming
+#august 25, 2024
+
+
+# Original tuple
+first_tuple=('a','b', 'c', 'd', 'e', 'f')
+
+
+# Convert tuple to list
+tuple_to_list= list(first_tuple)
+print(type(first_tuple))   # <class 'tuple'>
+print(type(tuple_to_list)) # <class 'list'>
+
+
+# Original list
+first_list=['a','b', 'c', 'd', 'e', 'f']
+
+# Convert list to tuple
+list_to_tuple= tuple(first_list)
+print(type(first_list))  # <class 'list'>
+print(type(list_to_tuple)) # <class 'tuple'>
+
