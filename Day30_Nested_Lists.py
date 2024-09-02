b22bdb4203942636b6f1111c31d1d8f69ea00288
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-09-02 20:26:41 +0530

    Add Day 30 Python code: Working with Nested Lists - Accessing Elements and Sub-lists

---
+++ Day30_Nested_Lists.py
@@ -0,0 +1,10 @@
+#Day30 of Python Programming
+#August 30, 2024
+
+
+la = [['a', 'b', 'c', 'd'], ['g', 'h', 'j', 'k'], ['n*m', 'p', 'q', 'z']]
+
+# Accessing elements from nested lists
+print(la[0][1])  # Output: 'b'
+print(la[2][1])  # Output: 'p'
+print(la[2])     # Output: ['n*m', 'p', 'q', 'z']
