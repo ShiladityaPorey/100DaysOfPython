7c392d4e27335026396ed40b68feae27b6ddc8a9
Shiladitya Porey <34293652+ShiladityaPorey@users.noreply.github.com>
2024-09-03 22:46:49 +0530

    Add Day 34 recap for Python Coding Challenge: Variables, Print, User Input

---
+++ day34_recap2_post1.py
@@ -0,0 +1,45 @@
+#September03, 2024
+
+# Variable naming examples
+variable_int = 10  # Integer
+variable_str = "Hello, World!"  # String
+variable_float = 10.0087  # Float
+variable_complex = 5 + 3j  # Complex number
+
+# Lists and Tuples
+list_example = [1, 3, 6, 'ty']  # List
+tuple_example = (1, 3, 6, 'ty')  # Tuple
+
+# Range
+range_example = range(2, 10, 2)  # Generates numbers from 2 to 8 with a step of 2
+
+# Boolean
+is_greater = 10 > 9  # Returns True
+is_equal = 10 == 9  # Returns False
+
+
+
+# Print examples
+print('This works.', variable_int)
+print("This also works: " + variable_str)
+print(f"Best practice with f-string: {variable_int}.")
+
+
+
+# User input with type casting
+integer_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = str(input("Enter a string: "))
+complex_input = complex(input("Enter a complex number: "))
+
+# Displaying the input and its type
+print(f"{integer_input} is of type {type(integer_input)}")
+print(f"{float_input} is of type {type(float_input)}")
+print(f"{string_input} is of type {type(string_input)}")
+print(f"{complex_input} is of type {type(complex_input)}")
+
+
+
+
+
+
